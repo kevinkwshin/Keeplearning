9475698c4d906db4599f9e2cc35c75d9464193f6
Keewon Shin <kevinkwshin@gmail.com>
2019-05-22 20:33:16 +0900

    Update normalize.py

--- normalize.py
+++ normalize.py
@@ -1,17 +1,23 @@
-   def _normalize(self,slice):
-        '''
-            input: unnormalized slice 
-            OUTPUT: normalized clipped slice
-        '''
-        b = np.percentile(slice, 99)
-        t = np.percentile(slice, 1)
-        slice = np.clip(slice, t, b)
-        image_nonzero = slice[np.nonzero(slice)]
-        if np.std(slice)==0 or np.std(image_nonzero) == 0:
-            return slice
-        else:
-            tmp= (slice - np.mean(image_nonzero)) / np.std(image_nonzero)
-            #since the range of intensities is between 0 and 5000 ,the min in the normalized slice corresponds to 0 intensity in unnormalized slice
-            #the min is replaced with -9 just to keep track of 0 intensities so that we can discard those intensities afterwards when sampling random patches
-            tmp[tmp==tmp.min()]=-9
-            return tmp
+def _normalize(self,slice):
+     '''
+         input: unnormalized slice 
+         OUTPUT: normalized clipped slice
+     '''
+     b = np.percentile(slice, 99)
+     t = np.percentile(slice, 1)
+     slice = np.clip(slice, t, b)
+     image_nonzero = slice[np.nonzero(slice)]
+     if np.std(slice)==0 or np.std(image_nonzero) == 0:
+         return slice
+     else:
+         tmp= (slice - np.mean(image_nonzero)) / np.std(image_nonzero)
+         #since the range of intensities is between 0 and 5000 ,the min in the normalized slice corresponds to 0 intensity in unnormalized slice
+         #the min is replaced with -9 just to keep track of 0 intensities so that we can discard those intensities afterwards when sampling random patches
+         tmp[tmp==tmp.min()]=-9
+         return tmp
+
+def z_normalization(img, num_channels):
+    for i in range(num_channels):
+        img[..., i] -= np.mean(img[..., i])
+        img[..., i] /= np.std(img[..., i])
+    return img
