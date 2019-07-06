2e8326b190d8f99e4ad54fc570ee81847c09b123
Keewon Shin <kevinkwshin@gmail.com>
2019-07-06 11:51:14 +0900

    Update load_data.py

--- load_data.py
+++ load_data.py
@@ -11,11 +11,8 @@ def load_dicom(path):
     return image
     
 def aview_img_to_nii(path_file,path_save):
-       
-    label = nib.load(path_label[0]).get_data().astype('float32')
+    label = nib.load(path_file).get_data().astype('float32')
     label = np.squeeze(label)
     label = np.fliplr(label)
-    labels = nib.Nifti1Image(label, affine=np.eye(4))
-    nib.save(labels,path_save)
-
-    return image,label
+    label = nib.Nifti1Image(label, affine=np.eye(4))
+    nib.save(label,path_save)
