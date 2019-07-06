d81398ba0eb8f5360bc2d7b99956d23800fd917a
Keewon Shin <kevinkwshin@gmail.com>
2019-07-06 18:37:00 +0900

    Delete list_shuffle.py

--- list_shuffle.py
+++
@@ -1,7 +0,0 @@
-def shuffle_list(list1,list2):
-    combined = list(zip(list1,list2))
-    random.shuffle(combined)
-    list1,list2 = zip(*combined)
-    list1 = list(list1)
-    list2 = list(list2)
-    return list1, list2
