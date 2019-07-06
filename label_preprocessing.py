a159e054c1209010260a78438c783bd283b3c9e0
Keewon Shin <kevinkwshin@gmail.com>
2019-07-06 18:38:38 +0900

    Update and rename label_binary_dilation.py to label_preprocessing.py

--- label_preprocessing.py
+++ label_preprocessing.py
@@ -1,4 +1,4 @@
-def binary_dilation(x, radius=3):
+def binary_dilation(x, radius=3): # 확장
     """ Return fast binary morphological dilation of an image.
     see `skimage.morphology.binary_dilation <http://scikit-image.org/docs/dev/api/skimage.morphology.html#skimage.morphology.binary_dilation>`_.
 
@@ -11,3 +11,6 @@ def binary_dilation(x, radius=3):
     mask = disk(radius)
     x = binary_dilation(x, selem=mask)
     return x 
+
+# multi-dimensional median filter # 레이블 스무딩
+label = scipy.ndimage.filters.median_filter(label,size=5)
