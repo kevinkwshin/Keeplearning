4aa0a74330f9aba5ad81572656cc010384a10e93
Keewon Shin <kevinkwshin@gmail.com>
2019-07-06 18:37:23 +0900

    Update and rename list_sort_nicely.py to list_preprocessing.py

--- list_preprocessing.py
+++ list_preprocessing.py
@@ -18,3 +18,10 @@ def sort_nicely(l):
     l.sort(key=alphanum_key)
     return l
     
+def shuffle_list(list1,list2):
+    combined = list(zip(list1,list2))
+    random.shuffle(combined)
+    list1,list2 = zip(*combined)
+    list1 = list(list1)
+    list2 = list(list2)
+    return list1, list2
