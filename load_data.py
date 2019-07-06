2f295ae2cc4b705326e81db88e7c63026903bc55
Keewon Shin <kevinkwshin@gmail.com>
2019-07-06 12:08:04 +0900

    Update load_data.py

--- load_data.py
+++ load_data.py
@@ -12,14 +12,3 @@ def load_dicom(path):
     image = reader.Execute()
     image = sitk.GetArrayFromImage(image)
     return image
-    
-def convert_aview_img_to_nii(path_file,path_save):  # Aview label to nifti label
-    label = nib.load(path_file).get_data().astype('float32')
-    label = np.squeeze(label)
-    label = np.fliplr(label)
-    label = nib.Nifti1Image(label, affine=np.eye(4))
-    nib.save(label,path_save)
-
-def convert_dicom_to_nii(path_file,path_save):
-    
-def convert_dicom_to_png(path_file,path_save):
