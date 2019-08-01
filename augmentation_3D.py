2a470e7dadb0e1e41447f70da77503d4a93f2ef3
Keewon Shin <kevinkwshin@gmail.com>
2019-08-01 16:21:34 +0900

    Update augmentation_3D.py

--- augmentation_3D.py
+++ augmentation_3D.py
@@ -1,5 +1,7 @@
 # 3D Augmentation using SimpleITK
 
+#############################################################################################################################
+# BSpline Transform
 def bspline_tranform_parameter(sitk_image, MeshSize=6, scale_distortion=4):
     
     transformDomainMeshSize=[MeshSize]*3
@@ -25,3 +27,35 @@ def bspline_tranform(sitk_input,tx,interpolator):
     aug_input = resampler.Execute(sitk_input)
     
     return aug_input
+
+image = sitk.ReadImage(path_image)
+tx = bspline_tranform_parameter(image)
+aug_image = bspline_tranform(image,tx,sitk.sitkBSpline)
+aug_image = sitk.GetArrayFromImage(aug_image)
+aug_mask = bspline_tranform(mask,tx,sitk.sitkNearestNeighbor)
+aug_mask = sitk.GetArrayFromImage(aug_mask)
+
+#############################################################################################################################
+# Affine Transform
+def affine_transform_parameter(translation_3d,size_scale,matrix_scale):
+    affine_center = (0,0,0)
+    affine_translation = (translation_3d[0]*np.random.random(),
+                          translation_3d[1]*np.random.random(),
+                          translation_3d[2]*np.random.random())
+    affine_scale = 1 + size_scale*np.random.random()
+    affine_matrix = [1,0,0,0,1,0,0,0,1] - np.random.random(9)*matrix_scale
+    return affine_matrix,affine_translation,affine_center,affine_scale
+
+def affine_transform(sitk_input,interpolator):
+    transform = sitk.AffineTransform(affine_matrix, affine_translation, affine_center)
+    transform.Scale(affine_scale)
+
+    reference_image = image
+    aug_input = sitk.Resample(sitk_input, reference_image, transform, interpolator)
+    
+    return aug_input
+
+affine_matrix,affine_translation,affine_center,affine_scale= affine_transform_parameter((0,10,10),0.05,0.05)
+
+aug_image = affine_transform(image,sitk.sitkBSpline)
+aug_mask  = affine_transform(mask,sitk.sitkNearestNeighbor)
