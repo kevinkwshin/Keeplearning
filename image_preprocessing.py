dce319602b9b91b3b960b1b66b75340c5da7f301
Keewon Shin <kevinkwshin@gmail.com>
2019-08-02 16:45:37 +0900

    Update image_preprocessing.py

--- image_preprocessing.py
+++ image_preprocessing.py
@@ -49,6 +49,7 @@ def image_preprocess_CT_uint8(img):
     return img
 
 def image_resize(data, img_dep=200., img_rows=200., img_cols=200.): # 3D image
+    # mode : 'constant’ for image, ‘nearest' for mask
     resize_factor = (img_dep/data.shape[0], img_rows/data.shape[1], img_cols/data.shape[2])
     data = ndimage.zoom(data, resize_factor, order=0, mode='constant', cval=0.0)
     return data
