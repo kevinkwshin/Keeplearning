cb4d1956f1331d4c4c9ad75c28b36f7dc5ab1a04
Keewon Shin <kevinkwshin@gmail.com>
2019-08-05 13:52:09 +0900

    Update label_processing.py

--- label_processing.py
+++ label_processing.py
@@ -77,3 +77,36 @@ def label_voxel_remover(results):
             results_processed[i,:,:,:,j][results_processed[i,:,:,:,j]!= cluster_max_label] = 0
             results_processed[i,:,:,:,j][results_processed[i,:,:,:,j]!= 0] = 1 # added
     return results_processed
+
+def label_onehot_encode(label,num_class):
+    
+    #input shape  (value  0,1,2,...)   : (image_depth,image_height,image_width)
+    #output shape (values 0,1) : (channel,image_depth,image_height,image_width)
+    
+    dimension = len(label.shape)
+    if dimension != 3:
+        print('error')
+    else:
+        label_onehot = torch.zeros((num_class,label.shape[0],label.shape[1],label.shape[2]))
+        for idx in range(num_class):
+            label_ = label.clone()
+            label_temp = label_
+            label_temp[label_temp!=idx+1.]=0.
+            label_temp[label_temp!=0.]=1.
+            label_onehot[idx] = label_temp
+    return label_onehot
+
+
+def label_onehot_decode(label_onehot):
+    #input shape (values 0,1) : (channel,image_depth,image_height,image_width)
+    #output shape (value  0,1,2,...)  : (image_depth,image_height,image_width)
+
+    # torch
+#     label_onehot = label_onehot.squeeze()
+#     value, indices = torch.max(label_onehot,0).astype('float32')
+
+    # numpy
+    label_onehot = label_onehot.squeeze().cpu().detach().numpy()
+    indices = np.argmax(label_onehot,0).astype('float32')
+
+    return indices#.squeeze()
