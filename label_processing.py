bd5a4dc4d3857d1869bae9f288e1bbb88e5eec60
Keewon Shin <kevinkwshin@gmail.com>
2019-08-16 15:12:18 +0900

    Update label_processing.py

--- label_processing.py
+++ label_processing.py
@@ -1,121 +1,102 @@
-import torch
-import numpy as np
+# import torch
+# import numpy as np
 
-def label_binary_dilation(x, radius=3): # 확장
-    """ Return fast binary morphological dilation of an image.
-    see `skimage.morphology.binary_dilation <http://scikit-image.org/docs/dev/api/skimage.morphology.html#skimage.morphology.binary_dilation>`_.
+# def label_binary_dilation(x, radius=3): # 확장
+#     """ Return fast binary morphological dilation of an image.
+#     see `skimage.morphology.binary_dilation <http://scikit-image.org/docs/dev/api/skimage.morphology.html#skimage.morphology.binary_dilation>`_.
 
-    Parameters
-    -----------
-    x : 2D array image.
-    radius : int for the radius of mask.
-    """
-    from skimage.morphology import disk, binary_dilation
-    mask = disk(radius)
-    x = binary_dilation(x, selem=mask)
-    return x 
+#     Parameters
+#     -----------
+#     x : 2D array image.
+#     radius : int for the radius of mask.
+#     """
+#     from skimage.morphology import disk, binary_dilation
+#     mask = disk(radius)
+#     x = binary_dilation(x, selem=mask)
+#     return x 
 
-# multi-dimensional median filter # 레이블 스무딩
-# label = scipy.ndimage.filters.median_filter(label,size=5)
+# # multi-dimensional median filter # 레이블 스무딩
+# # label = scipy.ndimage.filters.median_filter(label,size=5)
 
-def label_threshold(data,threshold):
-    data[data<=threshold]=0
-    data[data>threshold] =1
-    return data
-
-
-def label_crop_nonzero_slice(dimension,image,mask,*args):
-    if dimension==3:
-        xs,ys,zs = np.where(mask!=0)
-        image = image[min(xs):max(xs)+1,min(ys):max(ys)+1,min(zs):max(zs)+1]
-        mask  = mask[min(xs):max(xs)+1,min(ys):max(ys)+1,min(zs):max(zs)+1]
-    elif dimension==4:
-        xs,ys,zs,ts = np.where(mask!=0)
-        image = image[min(xs):max(xs)+1,min(ys):max(ys)+1,min(zs):max(zs)+1,min(ts):max(ts)+1]
-        mask  = mask[min(xs):max(xs)+1,min(ys):max(ys)+1,min(zs):max(zs)+1,min(ts):max(ts)+1]
-    else:
-        print('dimension should be 3 or 4')
-#     print(args[1])
-    return image,mask,args[0]
+# def label_threshold(data,threshold):
+#     data[data<=threshold]=0
+#     data[data>threshold] =1
+#     return data
 
 
-def label_crop_curriculum(image,mask,crop_shape):
-    # input shape
+# def label_crop_nonzero_slice(dimension,image,mask,*args):
+#     if dimension==3:
+#         xs,ys,zs = np.where(mask!=0)
+#         image = image[min(xs):max(xs)+1,min(ys):max(ys)+1,min(zs):max(zs)+1]
+#         mask  = mask[min(xs):max(xs)+1,min(ys):max(ys)+1,min(zs):max(zs)+1]
+#     elif dimension==4:
+#         xs,ys,zs,ts = np.where(mask!=0)
+#         image = image[min(xs):max(xs)+1,min(ys):max(ys)+1,min(zs):max(zs)+1,min(ts):max(ts)+1]
+#         mask  = mask[min(xs):max(xs)+1,min(ys):max(ys)+1,min(zs):max(zs)+1,min(ts):max(ts)+1]
+#     else:
+#         print('dimension should be 3 or 4')
+# #     print(args[1])
+#     return image,mask,args[0]
+
+
+# def label_crop_curriculum(image,mask,crop_shape):
+#     # input shape
     
-    image_ = image.copy()
-    mask_ = mask.copy()
-    mask_[mask_!=0]=0
-#     original_shape = image.shape
-#     D,H,W = crop_shape
-    orig_D, orig_H, orig_W = image.shape
-    crop_D, crop_H, crop_W = crop_shape
+#     image_ = image.copy()
+#     mask_ = mask.copy()
+#     mask_[mask_!=0]=0
+# #     original_shape = image.shape
+# #     D,H,W = crop_shape
+#     orig_D, orig_H, orig_W = image.shape
+#     crop_D, crop_H, crop_W = crop_shape
     
-    while np.any(mask_)==False:
-        depth_min =int(np.random.rand() * orig_D/2)
-        height_min =int(np.random.rand() * orig_H/2)
-        width_min = int(np.random.rand() * orig_W/2)
-        depth_max = depth_min + crop_D#crop_shape[0]
-        height_max =  height_min + crop_H#crop_shape[1]
-        width_max = width_min + crop_W#crop_shape[2]
+#     while np.any(mask_)==False:
+#         depth_min =int(np.random.rand() * orig_D/2)
+#         height_min =int(np.random.rand() * orig_H/2)
+#         width_min = int(np.random.rand() * orig_W/2)
+#         depth_max = depth_min + crop_D#crop_shape[0]
+#         height_max =  height_min + crop_H#crop_shape[1]
+#         width_max = width_min + crop_W#crop_shape[2]
         
-        if depth_max > orig_D:
-            depth_min -= (depth_max - orig_D)
-            depth_max = orig_D
-        image_ = image[depth_min:depth_max,height_min:height_max,width_min:width_max]
-        mask_ = mask[depth_min:depth_max,height_min:height_max,width_min:width_max]
-#         print(height_min,height_max,width_min,width_max)
-#     image_ = image_resize(image_,D,H,W,'constant')
-#     mask_ = image_resize(label_,D,H,W,'nearest')
+#         if depth_max > orig_D:
+#             depth_min -= (depth_max - orig_D)
+#             depth_max = orig_D
+#         image_ = image[depth_min:depth_max,height_min:height_max,width_min:width_max]
+#         mask_ = mask[depth_min:depth_max,height_min:height_max,width_min:width_max]
+# #         print(height_min,height_max,width_min,width_max)
+# #     image_ = image_resize(image_,D,H,W,'constant')
+# #     mask_ = image_resize(label_,D,H,W,'nearest')
     
-    return image_,mask_
+#     return image_,mask_
 
 
-def label_voxel_remover(results):
-    # Tensorflow 5D tensor (batch,img_dep,img_cols,img_rows,img_channel)
-    # Pytorch 5D tensor (batch,img_channel,img_dep,img_cols,img_rows)
-    results_processed = np.zeros((results.shape[0],results.shape[1],results.shape[2],results.shape[3],results.shape[4]))
+# def label_voxel_remover(results):
+#     # Tensorflow 5D tensor (batch,img_dep,img_cols,img_rows,img_channel)
+#     # Pytorch 5D tensor (batch,img_channel,img_dep,img_cols,img_rows)
+#     results_processed = np.zeros((results.shape[0],results.shape[1],results.shape[2],results.shape[3],results.shape[4]))
     
-    for i in range(results.shape[0]):
-        for j in range(results.shape[-1]):
-            results_processed[i,:,:,:,j] = measure.label(results[i,:,:,:,j], background = 0)
-            cluster = np.unique(results_processed[i,:,:,:,j])
-            cluster_max_voxels = 0
+#     for i in range(results.shape[0]):
+#         for j in range(results.shape[-1]):
+#             results_processed[i,:,:,:,j] = measure.label(results[i,:,:,:,j], background = 0)
+#             cluster = np.unique(results_processed[i,:,:,:,j])
+#             cluster_max_voxels = 0
 
-            for k in sorted(cluster)[1:]:
-                number_of_voxels = len(results_processed[i,:,:,:,j][results_processed[i,:,:,:,j] == k])
-                if cluster_max_voxels < number_of_voxels:
-                    cluster_max_voxels = number_of_voxels
-                    cluster_max_label = k
+#             for k in sorted(cluster)[1:]:
+#                 number_of_voxels = len(results_processed[i,:,:,:,j][results_processed[i,:,:,:,j] == k])
+#                 if cluster_max_voxels < number_of_voxels:
+#                     cluster_max_voxels = number_of_voxels
+#                     cluster_max_label = k
                     
-            results_processed[i,:,:,:,j][results_processed[i,:,:,:,j]!= cluster_max_label] = 0
-            results_processed[i,:,:,:,j][results_processed[i,:,:,:,j]!= 0] = 1 # added
-    return results_processed
+#             results_processed[i,:,:,:,j][results_processed[i,:,:,:,j]!= cluster_max_label] = 0
+#             results_processed[i,:,:,:,j][results_processed[i,:,:,:,j]!= 0] = 1 # added
+#     return results_processed
 
-
-def label_onehot_encode(label,num_class):
-    
-    #input shape  (value  0,1,2,...)   : (image_depth,image_height,image_width)
-    #output shape (values 0,1) : (num_class+1,image_depth,image_height,image_width)
-    #background is 0, so channel should be num_class+1
-    
-    dimension = len(label.shape)
-    if dimension != 3:
-        print('error')
-    else:
-        label_onehot = torch.zeros((num_class,label.shape[0],label.shape[1],label.shape[2]))
-        for idx in range(num_class):
-            label_temp = label.clone()
-            label_temp[label_temp!=idx]=0.
-            label_temp[label_temp!=0.]=1.
-            label_onehot[idx] = label_temp
-    return label_onehot
 
 # def label_onehot_encode(label,num_class):
     
 #     #input shape  (value  0,1,2,...)   : (image_depth,image_height,image_width)
 #     #output shape (values 0,1) : (num_class+1,image_depth,image_height,image_width)
 #     #background is 0, so channel should be num_class+1
-#     num_class += 1
     
 #     dimension = len(label.shape)
 #     if dimension != 3:
@@ -123,33 +104,39 @@ def label_onehot_encode(label,num_class):
 #     else:
 #         label_onehot = torch.zeros((num_class,label.shape[0],label.shape[1],label.shape[2]))
 #         for idx in range(num_class):
-#             if idx ==0:
-#                 #background
-#                 label_temp = label.clone()
-#                 label_temp[label_temp!=0]=100.
-#                 label_temp[label_temp==0]=1
-#                 label_temp[label_temp==100.]=0
-#                 label_onehot[idx] = label_temp
-#             else:
-#                 label_temp = label.clone()
-#                 label_temp[label_temp!=idx]=0.
-#                 label_temp[label_temp!=0.]=1.
-#                 label_onehot[idx] = label_temp
+#             label_temp = label.clone()
+#             label_temp[label_temp!=idx]=0.
+#             label_temp[label_temp!=0.]=1.
+#             label_onehot[idx] = label_temp
 #     return label_onehot
 
-
-def label_onehot_decode(label_onehot):
-    #input shape (values 0,1) : (channel,image_depth,image_height,image_width)
-    #output shape (value  0,1,2,...)  : (image_depth,image_height,image_width)
-
-    # torch
-#     label_onehot = label_onehot.squeeze()
-#     value, indices = torch.max(label_onehot,0).astype('float32')
-
-    # numpy
-    indices = np.argmax(label_onehot,0).astype('float32') # 0 for channel
+# # def label_onehot_encode(label,num_class):
     
-    return indices
+# #     #input shape  (value  0,1,2,...)   : (image_depth,image_height,image_width)
+# #     #output shape (values 0,1) : (num_class+1,image_depth,image_height,image_width)
+# #     #background is 0, so channel should be num_class+1
+# #     num_class += 1
+    
+# #     dimension = len(label.shape)
+# #     if dimension != 3:
+# #         print('error')
+# #     else:
+# #         label_onehot = torch.zeros((num_class,label.shape[0],label.shape[1],label.shape[2]))
+# #         for idx in range(num_class):
+# #             if idx ==0:
+# #                 #background
+# #                 label_temp = label.clone()
+# #                 label_temp[label_temp!=0]=100.
+# #                 label_temp[label_temp==0]=1
+# #                 label_temp[label_temp==100.]=0
+# #                 label_onehot[idx] = label_temp
+# #             else:
+# #                 label_temp = label.clone()
+# #                 label_temp[label_temp!=idx]=0.
+# #                 label_temp[label_temp!=0.]=1.
+# #                 label_onehot[idx] = label_temp
+# #     return label_onehot
+
 
 # def label_onehot_decode(label_onehot):
 #     #input shape (values 0,1) : (channel,image_depth,image_height,image_width)
@@ -160,15 +147,28 @@ def label_onehot_decode(label_onehot):
 # #     value, indices = torch.max(label_onehot,0).astype('float32')
 
 #     # numpy
+#     indices = np.argmax(label_onehot,0).astype('float32') # 0 for channel
     
-#     label_onehot[label_onehot>=0.5]=1.
-#     label_onehot[label_onehot<0.5]=0.
+#     return indices
+
+# # def label_onehot_decode(label_onehot):
+# #     #input shape (values 0,1) : (channel,image_depth,image_height,image_width)
+# #     #output shape (value  0,1,2,...)  : (image_depth,image_height,image_width)
+
+# #     # torch
+# # #     label_onehot = label_onehot.squeeze()
+# # #     value, indices = torch.max(label_onehot,0).astype('float32')
+
+# #     # numpy
     
-#     label = np.zeros((label_onehot.shape[1],label_onehot.shape[2],label_onehot.shape[3]))
-#     for idx in range(len(label_onehot)):
-#         label_temp = label_onehot[idx]
-#         label_temp[label_temp!=1.]=0.
-#         label_temp[label_temp==1.]=idx+1
-#         label += label_temp
-#     print(np.unique(label))
-#     return label
+# #     label_onehot[label_onehot>=0.5]=1.
+# #     label_onehot[label_onehot<0.5]=0.
+    
+# #     label = np.zeros((label_onehot.shape[1],label_onehot.shape[2],label_onehot.shape[3]))
+# #     for idx in range(len(label_onehot)):
+# #         label_temp = label_onehot[idx]
+# #         label_temp[label_temp!=1.]=0.
+# #         label_temp[label_temp==1.]=idx+1
+# #         label += label_temp
+# #     print(np.unique(label))
+# #     return label
