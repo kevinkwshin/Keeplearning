38f5dfe164910aa5c6a12c06a0b38781fd7464a4
Keewon Shin <kevinkwshin@gmail.com>
2019-08-28 10:49:00 +0900

    Update load_data.py

--- load_data.py
+++ load_data.py
@@ -1,25 +1,41 @@
 import SimpleITK as sitk
 import nibabel as nib
 
-def load_nii(path,return_array=True):
+def load_nii(path,return_info=False,return_array=True):
+    """
+    Parameter
+    - path
+    - return_info : If True, return will be image,spacing,origin
+    - return_array : Generally we return to array.
+    """
     image = sitk.ReadImage(path)
-    spacing = image.GetSpacing()
-    origin = image.GetOrigin()
-    
+   
+    if return_info == True:
+        spacing = image.GetSpacing()
+        origin = image.GetOrigin()
+
     if return_array == True:
         image = sitk.GetArrayFromImage(image).astype('float32')
 
-    return image,spacing,origin
+    if return_info == True:
+        return image,spacing,origin
+    else:
+        return image
 
-def load_dicom(path,return_array=True):
+def load_dicom(path,return_info=False,return_array=True):
     reader = sitk.ImageSeriesReader()
     dicom_names = reader.GetGDCMSeriesFileNames(path)
     reader.SetFileNames(dicom_names)
     image = reader.Execute()
-    
-    spacing = image.GetSpacing()
-    origin = image.GetOrigin()
+
+    if return_info == True:
+        spacing = image.GetSpacing()
+        origin = image.GetOrigin()
     
     if return_array == True:
         image = sitk.GetArrayFromImage(image)
-    return image,spacing,origin
+        
+    if return_info == True:
+        return image,spacing,origin
+    else:
+        return image
