3e3b5407b761e79d690624931fac6ea98c0dac9e
Keewon Shin <kevinkwshin@gmail.com>
2019-08-28 10:43:11 +0900

    Update load_data.py

--- load_data.py
+++ load_data.py
@@ -3,15 +3,23 @@ import nibabel as nib
 
 def load_nii(path,return_array=True):
     image = sitk.ReadImage(path)
+    spacing = image.GetSpacing()
+    origin = image.GetOrigin()
+    
     if return_array == True:
         image = sitk.GetArrayFromImage(image).astype('float32')
-    return image
+
+    return image,spacing,origin
 
 def load_dicom(path,return_array=True):
     reader = sitk.ImageSeriesReader()
     dicom_names = reader.GetGDCMSeriesFileNames(path)
     reader.SetFileNames(dicom_names)
     image = reader.Execute()
+    
+    spacing = image.GetSpacing()
+    origin = image.GetOrigin()
+    
     if return_array == True:
         image = sitk.GetArrayFromImage(image)
-    return image
+    return image,spacing,origin
