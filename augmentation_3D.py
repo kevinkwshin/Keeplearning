7ba288e7717d785d5279296145c09e7f4e08f01d
Keewon Shin <kevinkwshin@gmail.com>
2019-09-21 01:00:10 +0900

    Update augmentation_3D.py

--- augmentation_3D.py
+++ augmentation_3D.py
@@ -24,23 +24,26 @@ import SimpleITK as sitk
   
 def random_rotation_3D(x, rg, backend='keras', fill_mode='nearest', cval=0.):
     """
+    input : 5D tensor
     rg : (float,float,float)
     
     """
     if backend=='keras':
-         dep_index=0
-         row_index=1
-         col_index=2
-         channel_index=3
-    else:
          dep_index=1
          row_index=2
          col_index=3
-         channel_index=0
+         channel_index=4
+    else:
+         dep_index=2
+         row_index=3
+         col_index=4
+         channel_index=1
     
     theta1 = np.pi / 180 * np.random.uniform(-rg[0], rg[0])
     theta2 = np.pi / 180 * np.random.uniform(-rg[1], rg[1])
     theta3 = np.pi / 180 * np.random.uniform(-rg[2], rg[2])
+    print(theta1,theta2,theta3)
+
 
     rotation_matrix_z = np.array([[np.cos(theta1), -np.sin(theta1), 0, 0],
                                   [np.sin(theta1), np.cos(theta1), 0, 0],
