96ea1cacfd3ba0fde7d4623fc7b575441f6bd72c
Keewon Shin <kevinkwshin@gmail.com>
2019-11-01 10:09:34 +0900

    Update visualize.py

--- visualize.py
+++ visualize.py
@@ -5,7 +5,53 @@ from IPython.display import display
 import numpy as np
 from matplotlib.widgets import  RectangleSelector
 import matplotlib.patches as patches
-from sklearn.metrics import roc_curve
+
+import itertools
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+from sklearn.metrics import roc_auc_score, auc, roc_curve
+
+import matplotlib.pyplot as plt
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          reverse=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Oranges):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if reverse == True:
+        cm = cm[::-1,::-1]
+    cm_origin = cm.copy()
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt)+'\n({})'.format(cm_origin[i,j]),
+                 horizontalalignment="center",
+                 verticalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.tight_layout()
+
 
 def plot_auc_roc(label,pred):
     ground_truth_labels =label.ravel()
