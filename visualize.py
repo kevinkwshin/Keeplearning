b84dc7c81673cfae5ae3d206e744c3e28f2d72c7
Keewon Shin <kevinkwshin@gmail.com>
2019-11-01 14:43:48 +0900

    Update visualize.py

--- visualize.py
+++ visualize.py
@@ -7,38 +7,32 @@ from matplotlib.widgets import  RectangleSelector
 import matplotlib.patches as patches
 
 import itertools
+import matplotlib.pyplot as plt
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import classification_report
 from sklearn.metrics import roc_auc_score, auc, roc_curve
 
-import matplotlib.pyplot as plt
-def plot_confusion_matrix(cm, classes,
-                          normalize=False,
-                          reverse=False,
-                          title='Confusion matrix',
-                          cmap=plt.cm.Oranges):
+def plot_confusion_matrix(cm, classes, normalize=False, reverse=False,
+                          title='Confusion matrix', cmap=plt.cm.Oranges):
     """
     This function prints and plots the confusion matrix.
     Normalization can be applied by setting `normalize=True`.
     """
+    
     if reverse == True:
         cm = cm[::-1,::-1]
     cm_origin = cm.copy()
+    
     if normalize:
-        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-        print("Normalized confusion matrix")
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis];print("Normalized confusion matrix");
     else:
         print('Confusion matrix, without normalization')
-
-    print(cm)
-
-    plt.imshow(cm, interpolation='nearest', cmap=cmap)
-    plt.title(title)
-    plt.colorbar()
+    
     tick_marks = np.arange(len(classes))
-    plt.xticks(tick_marks, classes, rotation=45)
-    plt.yticks(tick_marks, classes)
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title);plt.colorbar();
+    plt.xticks(tick_marks, classes, rotation=45);plt.yticks(tick_marks, classes);
 
     fmt = '.2f' if normalize else 'd'
     thresh = cm.max() / 2.
